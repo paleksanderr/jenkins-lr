cb238ad408eb7ecffbae05ea591c9069d411abe2
Pawel <pawel.aleksander@gmail.com>
2022-02-01 17:38:44 +0100

    Debbuging Groovy

--- script.groovy
+++ script.groovy
@@ -1,56 +1,17 @@
-def gv
-
-pipeline {
-    agent any
-    parametrs {
-        choice(name: 'VERSION', choices: ['1.1.0', "1.2.0", "1.3.0"], description: '')
-        boolenParam(name: 'executeTest', defaultValue: true, description: '')
-    }
-
-    stages {
-        stage("init") {
-            steps {
-                script {
-                    gv = load "script.groovy"
-                }
-            }
-        }
-        stage('build') {
-             when {
-                expression {
-                    params.executeTests
-                }
-            }
-            
-            steps{
-                script {
-                    gv.buildApp()
-                }
-                echo 'Testing the app...'
-            }
-        }   
-        }
-        stage('test') {
-             
-            steps {
-                script {
-                    gv.testApp()
-                }
-                echo 'Deploying....'
-
-            }
-        stage('deploy') {
-             
-            steps {
-                script {
-                    gv.deployApp()
-                }
-                echo 'Deploying....'
-
-            }
-
-
-      
-    }
+def buildApp() {
+    echo "building the application..."
     
-}
+} 
+
+def testApp() {
+    echo "testing the application..."
+    
+} 
+
+def deployApp() {
+    echo "deploying the application..."
+    echo "deploying version ${params.VERSION}"
+    
+} 
+
+return this
